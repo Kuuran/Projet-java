6dbb4ee34efbbd2fbfeeaa38f3be8a26955df0c4
Felix <felix.jacquemin@etu.univ-lyon1.fr>
2018-05-09 22:32:21 +0200

    Ajout fonction create_program Ajout javadoc

--- src/project/Parser.java
+++ src/project/Parser.java
@@ -14,11 +14,15 @@ public class Parser {
     private XMLStreamReader xmlsr;
     private FileReader xmlfr;
 
-    public Parser(String name){
+    /**
+     * @brief Constructor of Parser class.
+     * @param filename
+     */
+    public Parser(String filename){
         xmlif = XMLInputFactory.newInstance();
 
         try {
-            xmlfr = new FileReader(name);
+            xmlfr = new FileReader(filename);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -30,28 +34,46 @@ public class Parser {
         }
     }
 
-    /*
-    fonction programme
-     case "title":
-                    //todo
-                    break;
-                case "sub-title" :
-                    //todo
-                    break;
-                case "desc":
-                    //todo
-                    break;
-                case "credits":
-                    //todo (gere les credits)
-                    break;
-                case "date":
-                    //todo
-                    break;
+    /**
+     * @brief Creates a new instance of program and broadcast classes and fill them with the informations read from the XML file.
+     * @throws XMLStreamException
      */
+    private void create_program() throws XMLStreamException {
+        int eventType = xmlsr.getEventType();
 
+        //todo cree un programm et un broadcast + ajoute start/stop/channelID a broadcast
 
+        while (xmlsr.hasNext() && (eventType != XMLEvent.END_ELEMENT || xmlsr.getLocalName().equals("//programme") == Boolean.FALSE)){
+            eventType = xmlsr.next();
 
+            if(eventType == XMLEvent.START_ELEMENT){
+                switch (xmlsr.getLocalName()){
+                    case "title":
+                        //todo
+                        break;
+                    case "sub-title" :
+                        //todo
+                        break;
+                    case "desc":
+                        //todo
+                        break;
+                    case "credits":
+                        //todo (gere les credits)
+                        break;
+                    case "date":
+                        //todo
+                        break;
 
+                }
+            }
+
+        }
+    }
+
+    /**
+     * @brief Creates a new instance of channel classe and fill it with the informations read from the XML file.
+     * @throws XMLStreamException
+     */
     private void create_channel() throws XMLStreamException {
         int eventType = xmlsr.getEventType();
 
@@ -61,27 +83,33 @@ public class Parser {
             eventType = xmlsr.next();
 
             if(eventType == XMLEvent.START_ELEMENT && xmlsr.getLocalName().equals("display-name")){
-                //todo
+                //todo modifie le display name du channel créé au dessus
             }
         }
     }
 
 
-
+    /**
+     * @brief Handles the XML event START_ELEMENT.
+     * @throws XMLStreamException
+     */
     private void start_element() throws XMLStreamException {
         switch(xmlsr.getLocalName()){
             case "channel":
                 create_channel();
                 break;
             case "programme":
-                //todo (cree emission + programme)
+                create_program();
                 break;
             default:
                 break;
         }
     }
 
-
+    /**
+     * @brief Main loop of the Parser class.
+     * @throws XMLStreamException
+     */
     public void run() throws XMLStreamException {
         int eventType = 0;
         int loop = 0;
